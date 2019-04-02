a56a53b9abe6465b052ca288489cd90132000147
Unknown <sam.lindsey@du.edu>
2019-04-02 16:47:23 -0600

    This is our first class example where we made the ball roll

---
+++ 1stClassExample/Assets/Scripts/playercontrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    //This moves our sphere by user input amount, Move Vertical is in z (up and down arrow), y is always zero
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
+
+        rb.AddForce(movement);
+    }
+}
